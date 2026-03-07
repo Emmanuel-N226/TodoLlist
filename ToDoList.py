c5a20196ca1223f734b1953b5d59e4677021cd30
Emmanuel Makhuba <nhlamulomakhuba1@gmail.com>
2026-03-07 09:33:02 +0200

    added new try&except code and new file also

--- ToDoList.py
+++ ToDoList.py
@@ -26,16 +26,16 @@ def add_task():
     #check if its an integer beimg entered
     if task.isdigit():
         print("Please enter words!!")
-        add_task()
+        return
     tasks.append(task)
-    pass
+    
 
 def view_task():
     if len(tasks) == 0:
         print("No task here!!")
     else:
         print(f"Here are your tasks\n{tasks}")
-    pass
+    
 
 def remove_tasks():
     if len(tasks) == 0:
@@ -48,39 +48,43 @@ def remove_tasks():
         done.append(task_remove)
         tasks.remove(task_remove)
         print(f"Your task {task_remove} has been completed and added to your done catalogue!!")
-    pass
+    
 
 def view_outstanding():
     view_task()
-    pass
+    
 
 def view_completed():
     print(f"here are you completed tasks:\n{done}")
-    pass
+    
 
 def exitout():
     global exit
     print("Closing ToDo list goodbye !!")
     exit = False
-    pass
+    
 
 #Daddy code holding all functions
 while exit:
-    choice = int(input("Hey welcome to your ToDo list what would you like to do?\n1.Add Task\n2.View Tasks\n3.Remove tasks\n4.View outsanding tasks\n5.View completed tasks\n6.Exit\nInput here: "))
-    if choice == 1:
-        add_task()
-    elif choice == 2:
-        view_task()
-    elif choice == 3:
-        remove_tasks()
-    elif choice == 4:
-        view_outstanding()
-    elif choice == 5:
-        view_completed()
-    elif choice == 6:
-        exitout()
-    else:
+    try:
+        choice = int(input("Hey welcome to your ToDo list what would you like to do?\n1.Add Task\n2.View Tasks\n3.Remove tasks\n4.View outsanding tasks\n5.View completed tasks\n6.Exit\nInput here: "))
+        if choice == 1:
+            add_task()
+        elif choice == 2:
+            view_task()
+        elif choice == 3:
+            remove_tasks()
+        elif choice == 4:
+            view_outstanding()
+        elif choice == 5:
+            view_completed()
+        elif choice == 6:
+            exitout()
+        else:
+            print("Please enter a valid option!!")
+    except ValueError:
         print("Please enter a valid option!!")
+
 
 
     
